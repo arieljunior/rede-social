bc94de1dc53cd4e6afe6438c38b01fbea0181aff
Ariel Junior <arieljuniorc@outlook.com>
2018-03-11 18:24:50 -0300

    Implementação da interface IRepositoryBase e da classe RepositoryBase (com erros)

---
+++ SocialNetwork/Repository/Repositories/RepositoryBase.cs
@@ -0,0 +1,38 @@
+﻿using Repository.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class RepositoryBase : IRepositoryBase<T> where T : class
+    {
+
+        public bool Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
