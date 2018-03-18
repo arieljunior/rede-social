a866d36fe3d35e199a3198a888a3f1966629e102
Ariel Junior <arieljuniorc@outlook.com>
2018-03-18 01:27:22 -0300

    Alteração na estrutura do projeto 2

---
+++ SocialNetwork/Data/Repositories/ProfileRepository.cs
@@ -0,0 +1,52 @@
+﻿using Data.Context;
+using Domain.Interfaces.Repositories;
+using DomainModel.Entities;
+using DomainModel.Entities.Profile;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class ProfileRepository : IRepository<ProfileModel>
+    {
+        private SocialNetworkContext _socialNetworkContext;
+        private DbSet<ProfileModel> _dbset;
+
+        public ProfileRepository(SocialNetworkContext socialNetworkContext)
+        {
+            _socialNetworkContext = socialNetworkContext;
+            _dbset = socialNetworkContext.Set<ProfileModel>();
+        }
+        public IEnumerable<ProfileModel> GetAll()
+        {
+            return null;
+        }
+
+        public ProfileModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(ProfileModel profile)
+        {
+            _dbset.Add(profile);
+            _socialNetworkContext.SaveChanges();
+
+            return true;
+        }
+
+        public bool UpDate(ProfileModel obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocialNetwork/Data/Repositories/SkillDb.cs
@@ -0,0 +1,61 @@
+﻿using DomainModel.Entities.Skill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.FakeDb
+{
+    public class SkillDb
+    {
+        private static List<SkillModel> SkillData = new List<SkillModel>();
+
+        public bool Save(SkillModel obj)
+        {
+            try
+            {
+                SkillData.Add(obj);
+
+                return true;
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
+        }
+
+        public List<SkillModel> GetAll()
+        {
+            return SkillData;
+        }
+
+        public SkillModel GetById(int id)
+        {
+            foreach (var skill in SkillData)
+            {
+                if (id == skill.IdSkill)
+                {
+                    return skill;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Remove(int id)
+        {
+            foreach (var skill in SkillData)
+            {
+                if (id == skill.IdSkill)
+                {
+                    SkillData.Remove(skill);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
