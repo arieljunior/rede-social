0e1a8bf283cd12ec97c7621729423efa3b9f16f0
Ariel Junior <arieljuniorc@outlook.com>
2018-04-12 18:15:24 -0300

    Alteração no AllProfiles

---
+++ SocialNetwork/Web/Scripts/MyScripts.js
@@ -0,0 +1,30 @@
+﻿function openEditPost(element) {
+    var id = element.id;
+    $('#msg-' + id).css({ 'display': 'none' });
+    $('#view-edit-'+id).css({'display':'block'});
+}
+
+function closeEditPost(element) {
+    var id = element.id;
+    $('#msg-' + id).css({ 'display': 'block' });
+    $('#view-edit-' + id).css({ 'display': 'none' });
+}
+
+function openEditComment(element) {
+    var id = element.id;
+
+    $('#comment-' + id).css({ 'display': 'none' });
+    $('#edit-comment-' + id).css({ 'display': 'block' });
+}
+
+function closeEditComment(element) {
+    var id = element.id;
+
+    $('#comment-' + id).css({ 'display': 'block' });
+    $('#edit-comment-' + id).css({ 'display': 'none' });
+}
+
+function openEditProfile() {
+    $('#details-profile').css({ 'display': 'none' });
+    $('#edit-profile').css({ 'display': 'block' });
+}
